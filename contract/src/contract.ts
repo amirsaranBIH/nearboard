8cf4716fc33a588dc26dcbcdff6fce2c116037e4
--local <--local>
2022-11-15 12:45:28 +0100

    A lot of improvements and bug fixes

--- contract/src/contract.ts
+++ contract/src/contract.ts
@@ -202,7 +202,7 @@ class Nearboard {
   }
 
   isUrl(url: string) {
-    const res = url.match(/(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g);
+    const res = url.match(/(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,8}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g);
     return res !== null;
   }
 
